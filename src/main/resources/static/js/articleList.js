22012c99eb7e4eab312a10421d2a036e01ddc691
tan199873 <1968397477@qq.com>
2019-07-18 07:09:41 +0800

    11

--- src/main/resources/static/js/articleList.js
+++ src/main/resources/static/js/articleList.js
@@ -10,12 +10,35 @@ function init(p,nav_id,article_title) {
             for (var i=0;i<data.navList.length;i++){
                 $("#nav_id").append("<option value="+data.navList[i].nav_id+">"+data.navList[i].nav_name+"</option>")
             }
+            var rows=data.rows;
             $("#rows").html("总共"+data.rows+"条记录");
-            $("#nowPage").html("");
-            $("#previousPage").html("");
-            $("#nextPage").html("");
-            $("#lastPage").html("");
-            $("#pageCount").html("");
+            $("#nowPage").html("当前为第"+(p+1)+"页");
+            if(p>0){
+
+                $("#firstPage").html("第一页");
+                $("#previousPage").html("上一页");
+                $("#firstPage").click(function(){
+                    init(p,nav_id,article_title);
+                })
+                $("#previousPage").click(function(){
+                    init(p-1,nav_id,article_title);
+                })
+            }
+            var  totalPages = parseInt((rows%10==0?rows/10:(rows / 10 +1)));
+            if(p<totalPages-1){
+                $("#nextPage").html("下一页");
+                $("#lastPage").html("最后一页");
+                $("#nextPage").click(function(){
+                    init(p+1,nav_id,article_title);
+                })
+                $("#lastPage").click(function(){
+                    init(totalPages-1,nav_id,article_title);
+                })}
+
+            $("#pageCount").html("共"+totalPages +"页");
+            if(totalPages>1){
+
+            }
 
         }
     })
